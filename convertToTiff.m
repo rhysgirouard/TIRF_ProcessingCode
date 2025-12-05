e095861aa28593df54d281ba31173315fa976628
rhysgirouard <rhys.girouard@gmail.com>
2025-12-05 12:58:14 -0800

    added comments and ability to process .tif files

--- convertToTiff.m
+++ convertToTiff.m
@@ -1,4 +1,20 @@
-function [convertedImagePath] = convertToTiff(sourceDir, destinationDir, imageName)
+function [convertedImagePath] = convertToTiff(sourceDir, destinationDir, imageName, ext)
+%convertToTiff converts an image or image stack into the TIFF file format
+%by using the BioFormats Importer in Fiji
+
+sourceImagePath = fullfile(sourceDir, imageName);
+
+% if source is already the correct type we can simply copy it into the new
+% location
+if strcmp(ext, '*.tif')
+    convertedImagePath = fullfile(destinationDir, imageName);
+    copyfile(sourceImagePath, convertedImagePath)
+    if ~isfile(convertedImagePath)
+        error(append('Failed to save ', convertedImagePath))
+    end
+
+    return
+end
 
 
 import ij.*;
@@ -6,13 +22,13 @@ import loci.plugins.BF.*;
 import ij.io.FileSaver.*;
 
 disp(['Opening image file: ', imageName]);
-imagePath = fullfile(sourceDir, imageName);
+
 
 % Open the image using Bio-Formats
 options = loci.plugins.in.ImporterOptions();
 options.setConcatenate(true)
 options.setOpenAllSeries(true)
-options.setId(imagePath);
+options.setId(sourceImagePath);
 imps = loci.plugins.BF.openImagePlus(options);  % This will return an array of ImagePlus objects
 imp = imps(1); % There has to be a way to skip this step
 
@@ -23,7 +39,7 @@ end
 % Saving the image
 disp(['Saving to: ', destinationDir]);
 
-newImageName = extractBefore(imageName, '.nd2');
+newImageName = extractBefore(imageName, ext(2:end));
 convertedImagePath = fullfile(destinationDir, [newImageName, '.tif']);
 fs = ij.io.FileSaver(imp);
 fs.saveAsTiff(convertedImagePath);
