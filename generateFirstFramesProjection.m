064a0a273fe82044829ba54185087fb881848d51
rhysgirouard <rhys.girouard@gmail.com>
2026-01-21 13:22:59 -0800

    check for trackmate data and clear xy intitialization

--- generateFirstFramesProjection.m
+++ generateFirstFramesProjection.m
@@ -1,30 +1,32 @@
-function [OutputFilePath] = generateFirstFramesProjection(folderPath, OriginalStack)
-%generateFirstFramesProjection preloads the image and generates a Z-projection for tracking
-%   Loads in the image as a 3D uint16 array for future use and then sums
-%   the first 3 frames together to create a background "averaged" image for
-%   use in tracking. The image for tracking needs to have multiple frames
-%   because Trackmate is set up for motion tracking. 
+function [OutputFilePath] = generateFirstFramesProjection(folderPath, firstFrames)
+%generateFirstFramesProjection generates a Z-projection for tracking
+%   Sums the first 3 frames together to create a background "averaged" 
+%   image for use in tracking. The image for tracking needs to have 
+%   multiple frames because Trackmate is set up for motion tracking. 
 
 OutputFileName = 'First3frames.tif';
-OutputFilePath = fullfile(folderPath,OutputFileName);
-NoProjection = 3;
-minimumValue = min(OriginalStack,[],"all");
+OutputFilePath = fullfile( folderPath, OutputFileName );
+projectionSize = size( firstFrames, 3 );
+
+% Check for negative values from subtraction. 
+minimumValue = min( firstFrames, [], "all" );
 if minimumValue < 0
-    OriginalStack = OriginalStack + -1*minimumValue;
+    firstFrames = firstFrames + -1 * minimumValue;
 end
-OriginalStack = cast(OriginalStack, 'uint16');
+firstFrames = cast(firstFrames, 'uint16');
 
 
 % Create a Z-projection(sum of intensities) of the first three frames to
 %average out noise
-ZProjection=OriginalStack(:,:,1);
-for i=2:NoProjection
-    ZProjection=ZProjection+OriginalStack(:,:,i);
+ZProjection=firstFrames( :, :, 1 );
+for i = 2 : projectionSize
+    ZProjection = ZProjection + firstFrames( :, :, i );
 end
 
 % Create an image stack with 5 copies of the Zprojection for "motion
 % tracking" in Trackmate
-for i=1:5
-    imwrite(ZProjection,OutputFilePath,'WriteMode','append','Compression','none');
+for i = 1 : 5
+    imwrite( ZProjection, OutputFilePath, 'WriteMode', 'append',...
+        'Compression', 'none' );
 end
 end
