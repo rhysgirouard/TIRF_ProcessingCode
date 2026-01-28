c59521f9a23a0344540313a3f17fe00ce70e8e4c
rhysgirouard <rhys.girouard@gmail.com>
2026-01-28 15:42:13 -0800

    added parsing for spaces in fiji path PC version and possible use of --script if --run fails

--- saveTrackXML.m
+++ saveTrackXML.m
@@ -13,12 +13,23 @@ args = sprintf("imagePath=%s,folderPath=%s,radius=%s,quality=%s,channel=%s",...
 if ismac()
     cmd = sprintf('"%s" --headless --run "%s" "%s"', executablePath, scriptPath, args);
 elseif ispc()
-    cmd = sprintf('%s --headless --console --run "%s" "%s"', executablePath, scriptPath, args);
+    cmd = sprintf('"%s" --headless --console --run "%s" "%s"', executablePath, scriptPath, args);
 else
     warning("WARNING! unsupported OS")
     cmd = sprintf('"%s" --headless --run "%s" "%s"', executablePath, scriptPath, args);
 end
-status = system(cmd);
+try
+    status = system(cmd);
+catch ME
+    if ismac() 
+        cmd = sprintf('"%s" --headless --script "%s" "%s"', executablePath, scriptPath, args);
+    elseif ispc()
+        cmd = sprintf('"%s" --headless --console --script "%s" "%s"', executablePath, scriptPath, args);
+    else
+        cmd = sprintf('"%s" --headless --script "%s" "%s"', executablePath, scriptPath, args);
+    end
+    status = system(cmd);
+end
 if status ~= 0
     error('TrackMate failed to run.');
 end
